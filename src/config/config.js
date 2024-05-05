a0ce55c1de74337f0b839f16ca3c83d2003c0377
Farooque Reza <rezafarooque@outlook.com>
2024-05-05 21:06:41 +0530

    add user and video model

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,11 @@ conf();
 const _config = {
   port: process.env.PORT || 3000,
   mongodbURI: process.env.MONGODB_URI,
-  jwtSecret: process.env.JWT_SECRET,
   corsOrigins: process.env.CORS_ORIGINS,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
 };
 
 export const config = Object.freeze(_config);
